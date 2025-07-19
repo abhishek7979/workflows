1fc3bf30fa7af10d04d2e8943caff8975eb4731e
abhishek7979 <49263031+abhishek7979@users.noreply.github.com>
2025-07-19 18:20:01 +0530

    trying this file

--- test.js
+++ test.js
@@ -2,7 +2,8 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  iterations: 10, // number of requests
+  vus: 1, 
+  iterations: 3,
 };
 
 export default function () {
